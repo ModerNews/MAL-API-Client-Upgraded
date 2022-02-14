6573a0a06cbc55312ca1fc5c0d20c140d5e2ffda
ModerNews <grzesiujag@poczta.fm>
2022-02-14 23:31:30 +0100

    removing myst_parser from sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser',
-              'sphinx.ext.duration',
+extensions = ['sphinx.ext.duration',
               'sphinx.ext.doctest',
               'sphinx.ext.autodoc',
               'enum_tools.autoenum',

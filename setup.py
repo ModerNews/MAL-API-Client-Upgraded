13f7eece6df6853d2bfd402ffc1246d1b19d9957
gruzin <grzesiujag@poczta.fm>
2022-11-04 20:05:11 +0100

    Updating version info for pypi release 1.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="malclient-upgraded",
-    version="1.3",
+    version="1.3.1",
     author="ModerNews",
     author_email="polski.gruzin.biz@gmail.com",
     description=
